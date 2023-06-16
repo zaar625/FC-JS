a4eeb4a2cecb2bf124bc5ceac0ea6444e3803c7c
이상윤 <zaar625@gmail.com>
2023-06-17 03:15:25 +0900

    filter

---
+++ section3/filter.mjs
@@ -0,0 +1,32 @@
+const products = [
+    {name: '반팔티', price: 15000},
+    {name: '긴팔티', price: 20000},
+    {name: '핸드폰케이스', price: 15000},
+    {name: '후드티', price: 30000},
+    {name: '바지', price: 25000}
+  ];
+
+  let under20000 = [];
+  for(const p of products) {
+    if(p.price < 20000) under20000.push(p);
+  }
+
+//   console.log(...under20000)
+
+// 위 코드를 filter함수를 만들어 리팩토링 해보겠습니다.
+const filter = (f, iter) => {
+    let res =[];
+    for(const a of iter) {
+      if(f(a)) res.push(p);
+    }
+    return res;
+}
+
+filter(p => p.price < 20000 , products)
+filter (n => n % 2, function *(){
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+}()) // [1,3,5]
